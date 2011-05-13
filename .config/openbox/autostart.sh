f60dc3ceaad5ab3ed64c28e016dc61bd0f5fd0a3
Mike Stegeman <mrstegeman@gmail.com>
2011-05-13 08:01:35 -0400

    a few fixes to autostart, a couple more openbox menu entries

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -2,9 +2,7 @@
 
 nitrogen --restore &
 (sleep 2 && stalonetray) &
-sh /home/michael/Programs/scripts/start_conky.sh &
+sh /home/michael/bin/start_conky.sh &
 #nm-applet --sm-disable &
 blueman-applet &
-if [ $? -eq 0 ]; then
-    mpd
-fi
+mpd &
