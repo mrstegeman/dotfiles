bfbd197126f9e4cd92e6a448a461944db79ca229
Mike Stegeman <mrstegeman@gmail.com>
2011-06-05 10:45:33 -0400

    new useragent string for uzbl

--- .config/uzbl/scripts/history.sh
+++ .config/uzbl/scripts/history.sh
@@ -6,7 +6,7 @@
 
 # remove duplicate entries
 trans=$(echo "$UZBL_URI" | sed 's_\/_\\\/_g')
-sed -i "/$trans /d" "$UZBL_HISTORY_FILE"
+sed -i "/ $trans /d" "$UZBL_HISTORY_FILE"
 
 # now add the link to the history file
 echo "$( date +'%Y-%m-%d %H:%M:%S' ) $UZBL_URI $UZBL_TITLE" >> "$UZBL_HISTORY_FILE"
