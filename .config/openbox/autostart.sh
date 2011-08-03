882943dac289ab1c346761dcefc982c5c9b42980
Mike Stegeman <mrstegeman@gmail.com>
2011-08-03 19:39:55 -0400

    removing openbox configs, switched to wmfs

--- .config/openbox/autostart.sh
+++
@@ -1,8 +0,0 @@
-. $GLOBALAUTOSTART &
-
-nitrogen --restore &
-(sleep 2 && stalonetray) &
-sh /home/michael/bin/start_conky.sh &
-#nm-applet --sm-disable &
-blueman-applet &
-mpd &
--- .config/wmfs/autostart.sh
+++ .config/wmfs/autostart.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+xmodmap ~/.Xmodmap &
+xbindkeys &
+/home/michael/git/nullfs/nul1fs /home/michael/.Skype/Logs &
+#sh /home/michael/bin/start_conky.sh &
+nm-applet --sm-disable &
+blueman-applet &
+mpd &
 feh --bg-tile /home/michael/wallpapers/current.jpg
 conky | while read -r conky_out; do wmfs -s "$conky_out"; done
